0809c9204e35c0ec74a2e54277efa47e6de7cc62
យឹម សុភ័ក្រ្ត <sopheak@gmail.com>
2013-07-22 09:19:09 +0700

    Pheak commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sopheak::Application.config.secret_key_base = 'f276eabcacc55a39bf41db6bfb08156030530da93160e635cac509f56282584f9d3e60bd32a6967c33b5c16406193dc41254b811783f43bef9161b8a6c90fefa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sopheak::Application.config.session_store :cookie_store, key: '_sopheak_session'
